90a10aec267e8d1188c906defa7cbc72e9adf52a
Patricio Baliña <paddybalinia@gmail.com>
2024-09-02 11:10:22 -0300

    feat:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		host: true, // Escuchar en todas las interfaces
-		// port: 3000, // Puedes cambiar el puerto si lo necesitas
-	},
-	site: 'https://cs-arq.vercel.app/', // Write here your website url
+	site: 'https://newsite-tan.vercel.app/', // Write here your website url
 });
