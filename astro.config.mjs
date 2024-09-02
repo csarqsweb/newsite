e678fedb07039c49fb677daccedfe76250d2d27a
Patricio Baliña <paddybalinia@gmail.com>
2024-09-02 11:13:59 -0300

    feat: prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://newsite-tan.vercel.app/', // Write here your website url
+	site: 'https://newsite-tan.vercel.app', // Write here your website url
 });
