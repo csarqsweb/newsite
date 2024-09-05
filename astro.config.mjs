5f7c28c073cc69e084783aaed66ba26e4a00aae9
Patricio Baliña <paddybalinia@gmail.com>
2024-09-05 14:36:58 -0300

    feat: ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://newsite-tan.vercel.app', // Write here your website url
+	server: {
+		host: true, // Esto permite que el servidor escuche en la red local
+	},
 });
