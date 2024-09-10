6ff6e030327cb7179d00d4e927d1db7265c8e0ef
Patricio Baliña <paddybalinia@gmail.com>
2024-09-10 15:31:14 -0300

    fix: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://newsite-tan.vercel.app', // Write here your website url
+	site: 'https://csarqs.vercel.app', // Write here your website url
 	server: {
 		host: true, // Esto permite que el servidor escuche en la red local
 	},
