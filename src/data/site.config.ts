ea2294605b6a26497e19a62683c0fcaa1891eac5
Patricio Baliña <paddybalinia@gmail.com>
2024-09-10 14:24:59 -0300

    feat: congif SEO

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -2,6 +2,7 @@ interface SiteConfig {
 	author: string;
 	title: string;
 	description: string;
+	tags: string;
 	lang: string;
 	ogLocale: string;
 	shareMessage: string;
@@ -11,7 +12,9 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	author: 'Patricio Baliña', // Site author
 	title: 'CS Arquitectura', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	description:
+		'Somos un estudio de arquitectura dedicado a transformar ideas en espacios únicos y funcionales. Combinamos creatividad, innovación y experiencia para diseñar ambientes que reflejan tu visión y necesidades.', // Description to display in the meta tags
+	tags: 'estudio de arquitectura, diseño arquitectónico, transformación de espacios, innovación en arquitectura, diseño personalizado, experiencia en arquitectura',
 	lang: 'es-ES',
 	ogLocale: 'es_ES',
 	shareMessage: 'Share this post', // Message to share a post on social media
